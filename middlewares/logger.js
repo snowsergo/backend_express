cf5cdf82a4f5fdaf0e392b68a9cc5fd6c72b7b1c
Sergey Stupnikov <sergey.stupnikov.59@yandex.ru>
2020-02-27 00:14:34 +0500

    clear master

--- middlewares/logger.js
+++
@@ -1,6 +0,0 @@
-const logger = (req,res,next) =>{
-  console.log(Date.now(), req.method, req.path);
-  next();
-}
-
-module.exports = logger;
--- routes/any.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router(); // создали роутер
-
-router.get('/', (req, res, next) => {
-  return next ({status: 401, message:{ message: "где ресурс" }})
-});
-
-module.exports = router; // экспортировали роутер
